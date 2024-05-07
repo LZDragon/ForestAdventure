c73fe7b3a8095c370ddefbe5561dc610aad114e1
Eli <elizatelkamp@gmail.com>
2024-05-06 21:44:36 -0400

    Fixed PlayerMovement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(HealthComponent))]
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private HealthComponent playerHealthComponent;
+    [SerializeField] private GameObject playerModel;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void Attack()
+    {
+        if (Physics.Raycast(transform.position, playerModel.transform.forward,out RaycastHit hitInfo, 1f, 1 << 7))
+        {
+            hitInfo.transform.GetComponent<Enemy>().ReceiveHit(Random.Range(30f,50f));
+        }
     }
 }
