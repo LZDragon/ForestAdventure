89ff7fb590077a355545c1a6fb3bdb0b70840a65
Eli <elizatelkamp@gmail.com>
2024-05-07 12:40:48 -0400

    Added Health bar

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(HealthComponent))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private HealthComponent playerHealthComponent;
     [SerializeField] private GameObject playerModel;
+    [SerializeField] private Slider playerHealthBar;
     // Start is called before the first frame update
     void Start()
     {
         playerHealthComponent.HandleOnKilled += OnKilled;
         playerHealthComponent.HandleHealthUpdated += OnHealthUpdated;
+        playerHealthBar.maxValue = playerHealthComponent.Health;
+        playerHealthBar.value = playerHealthComponent.Health;
     }
 
     public void Attack()
     {
-        if (Physics.Raycast(transform.position, playerModel.transform.forward,out RaycastHit hitInfo, 1f, 1 << 7))
+        if (Physics.SphereCast(transform.position,5f, playerModel.transform.forward, out RaycastHit hitInfo, 5f, 1 << 7))
         {
             hitInfo.transform.GetComponent<Enemy>().ReceiveHit(Random.Range(30f,50f));
         }
@@ -25,7 +29,7 @@ public class Player : MonoBehaviour
 
     void OnHealthUpdated(float updatedHealth)
     {
-        
+        playerHealthBar.value = updatedHealth;
     }
 
     void OnKilled()
